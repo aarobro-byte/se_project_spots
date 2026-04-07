988f187fb90481f036e1f2843047b8cb27416188
Aaron <brocklebank.aaron@gmail.com>
2026-04-07 01:08:51 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edit_profile-modal");
+const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = document.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostClosebtn = newPostModal.querySelector(".modal__close-btn");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal__is-opened");
@@ -19,5 +19,5 @@ newPostBtn.addEventListener("click", function () {
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostCloseBtn.classList.remove("modal__is-opened");
+  newPostModal.classList.remove("modal__is-opened");
 });
