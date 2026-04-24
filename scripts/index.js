3d11bff4b9ff2c344fff7ea8f168167233b8359a
Aaron <brocklebank.aaron@gmail.com>
2026-04-23 22:57:59 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,34 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = document.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const addCardImageInputForm = newPostModal.querySelector(".modal__form");
+const editProfileCardImageLink = editProfileModal.querySelector(
+  "#profile-card-image"
+);
+const editProfileCaptionLink = editProfileModal.querySelector(
+  "profile-caption-link"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEL = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const ProfileCardImageLink = document.querySelector("profile__Card-image");
+const ProfileCaptionLink = document.querySelector("profile__Caption-link");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameInput.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContext;
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -21,3 +43,19 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEL.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  ProfileCardImageLink.textContext = addCardImageLink.value;
+  ProfileCaptionLink.textContext = addCaptionLink.value;
+}
+
+addCardImageInputForm.addEventListener("sumbit", handleCardInputFormSubmit);
